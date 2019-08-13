fb6e7a0931e051ac50bb78f64cd115850ac66e76
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-08-13 17:07:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function logDriverNames(arr) {
+  arr.forEach(function (el) {
+    console.log(el.name);
+  })
+}
+function logDriversByHometown(driver, location) {
+  driver.forEach(function(el) {
+    if(el.hometown === location){
+      console.log(el.name);
+    }
+  })  
+}
+function driversByRevenue(driver) {
+  const driverCopy = [...driver];
+  return driverCopy.sort(function(a,b){
+    return a.revenue - b.revenue;
+  })
+}
+function driversByName(driver) {
+  const driverCopy = [...driver];
+  return driverCopy.sort(function(a,b) {
+    return a.name.localeCompare(b.name);
+  })
+}
+
+function totalRevenue(driver) {
+  return driver.reduce((acc,cur) => acc + cur.revenue,0);
+}
+function averageRevenue(driver) {
+  return totalRevenue(driver)/driver.length;
+}
