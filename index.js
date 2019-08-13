d777b65d5a70409a75bba8287e616fecbf204768
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-08-13 17:02:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+function logDriverNames(arr) {
+  arr.forEach(function (el) {
+    console.log(el.name);
+  })
+}
+function logDriversByHometwon(driver, location) {
+  driver.forEach(function(el) {
+    if(el.location === location){
+      console.log(el.name);
+    }
+  })  
+}
+function driversByRevenue(driver) {
+  const driverCopy = [...driver];
+  return driverCopy.sort(function(a,b){
+    return a.revenue - b.revenue;
+  })
+}
